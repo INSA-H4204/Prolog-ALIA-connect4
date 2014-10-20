25710dad078f11f3458c53ffbb41995dacd9f5ea
gcbounous <gccbounous@gmail.com>
2014-10-20 09:19:03 +0200

    Update puissance4.pl
    corretion et ameilloration du code

--- puissance4.pl
+++ puissance4.pl
@@ -12,8 +12,8 @@ p([['_','_','_','_','_','_','_','_'],
 
 liste:- liste(0,0).
 
-liste(8,8):- !.
-liste(8,L):- write('
-'), Q is L+1, liste(0,Q).
-liste(C,L):- p(Liste), nth0(C,Liste,E), nth0(L,E,X), write(X),write(' '),P is C+1, liste(P,L).
+liste(8,8):- writeElement(8,8), !.
+liste(8,L):- writeElement(8,L), writeln(''), Q is L+1, liste(0,Q).
+liste(C,L):- writeElement(C,L), write(' '),P is C+1, liste(P,L).
 
+writeElement(C,L):- p(Liste), nth0(C,Liste,E), nth0(L,E,X), write(X).
