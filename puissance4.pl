77040b3e1a22a828c86f947ed7e3db06b3b36509
gcbounous <gccbounous@gmail.com>
2014-10-20 09:34:31 +0200

    Update puissance4.pl

--- puissance4.pl
+++ puissance4.pl
@@ -1,6 +1,6 @@
-:- dynamic 	p/1.
+:- dynamic p/1.
 
-% Il faut tout remplir
+% space de jeu vide
 p([['_','_','_','_','_','_','_','_'],
 ['_','_','_','_','_','_','_','_'],
 ['_','_','_','_','_','_','_','_'],
@@ -10,10 +10,10 @@ p([['_','_','_','_','_','_','_','_'],
 ['_','_','_','_','_','_','_','_'],
 ['_','_','_','_','_','_','_','_']]).
 
+% affiche l'espace de jeu en 'console'
 liste:- liste(0,0).
-
-liste(8,8):- writeElement(8,8), !.
-liste(8,L):- writeElement(8,L), writeln(''), Q is L+1, liste(0,Q).
+liste(7,7):- writeElement(7,7), !.
+liste(7,L):- writeElement(7,L), writeln(''), Q is L+1, liste(0,Q).
 liste(C,L):- writeElement(C,L), write(' '),P is C+1, liste(P,L).
 
 writeElement(C,L):- p(Liste), nth0(C,Liste,E), nth0(L,E,X), write(X).
